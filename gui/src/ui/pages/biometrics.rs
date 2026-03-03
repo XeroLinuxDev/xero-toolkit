3b0c10f4a38b15fe2b181998fea408a9ff06c2bf
DarkXero-dev <steve@techxero.com>
2026-03-03 17:21:39 +0200

    Howdy & Tweaks

--- gui/src/ui/pages/biometrics.rs
+++ gui/src/ui/pages/biometrics.rs
@@ -2,7 +2,6 @@
 //!
 //! Handles:
 //! - Fingerprint reader setup (xfprintd-gui)
-//! - Howdy facial recognition setup (xero-howdy-qt)
 
 use crate::core;
 use crate::ui::task_runner::{self, Command, CommandSequence};
@@ -16,7 +15,6 @@ use std::process::{Command as StdCommand, Stdio};
 pub fn setup_handlers(page_builder: &Builder, _main_builder: &Builder, window: &ApplicationWindow) {
     crate::ui::dialogs::button_info::attach_to_builder(page_builder, window.upcast_ref());
     setup_fingerprint(page_builder, window);
-    setup_howdy(page_builder, window);
 }
 
 /// Helper to update button appearance based on installation status and control uninstall button visibility
@@ -123,56 +121,3 @@ fn setup_fingerprint(page_builder: &Builder, window: &ApplicationWindow) {
     });
 }
 
-fn setup_howdy(page_builder: &Builder, _window: &ApplicationWindow) {
-    let btn_howdy_setup = extract_widget::<gtk4::Button>(page_builder, "btn_howdy_setup");
-
-    // Disable Howdy setup as it's not ready yet
-    btn_howdy_setup.set_sensitive(false);
-}
-
-// fn setup_howdy(page_builder: &Builder, window: &ApplicationWindow) {
-//     let btn_howdy_setup = extract_widget::<gtk4::Button>(page_builder, "btn_howdy_setup");
-
-//     // Initial check
-//     let is_installed = core::is_package_installed("xero-howdy-qt");
-//     update_button_state(&btn_howdy_setup, is_installed);
-
-//     // Update on window focus (e.g. after installation completes)
-//     let btn_clone = btn_howdy_setup.clone();
-//     window.connect_is_active_notify(move |window| {
-//         if window.is_active() {
-//             let is_installed = core::is_package_installed("xero-howdy-qt");
-//             update_button_state(&btn_clone, is_installed);
-//         }
-//     });
-
-//     let window = window.clone();
-//     btn_howdy_setup.connect_clicked(move |_| {
-//         info!("Biometrics: Howdy setup button clicked");
-
-//         // Check again at click time
-//         if core::is_package_installed("xero-howdy-qt") {
-//             info!("Launching xero-howdy-qt...");
-//             if let Err(e) = StdCommand::new("xero-howdy-qt")
-//                 .stdin(Stdio::null())
-//                 .stdout(Stdio::null())
-//                 .stderr(Stdio::null())
-//                 .spawn()
-//             {
-//                 error!("Failed to launch xero-howdy-qt: {}", e);
-//             }
-//         } else {
-//             let commands = CommandSequence::new()
-//                 .then(
-//                     Command::builder()
-//                         .aur()
-//                         .args(&["-S", "--noconfirm", "--needed", "xero-howdy-qt"])
-//                         .description("Installing Xero Howdy Qt...")
-//                         .build(),
-//                 )
-//                 .build();
-
-//             task_runner::run(window.upcast_ref(), commands, "Install Xero Howdy Qt");
-//         }
-//     });
-// }
